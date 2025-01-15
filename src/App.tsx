43215b88956134a8cd31568e5c54feeb802eac0f
irdepla <rixsiboyevislombek@gmail.com>
2025-01-15 16:34:08 +0500

    finished

---
+++ src/App.tsx
@@ -0,0 +1,133 @@
+function App() {
+  // Exercise 1
+  // function getReverse(array:number[]){
+  //   return array.reverse()
+  // }
+  // console.log(getReverse([1,2,3,4,5]));
+
+  // Exercise 2
+
+  // function Uppercase(str: string) {
+  //   return str.toUpperCase();
+  // }
+
+  // console.log(Uppercase("i am student at university"));
+
+
+  // Exercise 3
+
+
+  // function addElement(a:number[], b:number) : number[] {
+  //   a.push(b)
+  //   return a
+  // }
+
+  // console.log(addElement([1,2,3], 8))
+
+
+  // Exercise 4
+
+
+  // function length(a:number[]){
+  //   return a.length
+  // }
+
+  // console.log(length([4,6,12,7,9]));
+
+
+  // Exercise 5
+
+  // function replaceWord(a : string){
+  //  let newStr =  a.replace("World", "JavaScript")
+  //   return newStr;
+  // }
+
+  // console.log(replaceWord("Hello World"));
+
+  // Exercise 6
+
+  // let arr = [8, 5, 6]
+  // console.log(arr[0])
+
+
+  // Exercise 7
+
+  // let str = "Uzbekistan"
+  // return str.length
+
+  // Exercise 8
+
+  // let arr = [2,3,4]
+  // return arr.unshift(1)
+
+  // Exercise 9
+
+  // let str = "FEVLKJSCNAMCEWUYJVBI"
+
+  // console.log(str.toLowerCase());
+
+
+  // Exercise 10
+
+  // let arr = ["jsenvks", "whes", "dgcsh"]
+  // return arr.join()
+
+
+  // Exercise 11
+
+  // let arr = [5,2,9,1,7]
+  // console.log(arr.sort());
+  
+
+  // Exercise 12
+
+  // let str = "Olma, Banan, Anjir"
+  //  let arr = str.split(",")
+  // console.log(arr);
+
+
+  // Exercise 13
+
+  // let arr = [1,2,3,4,5]
+  // console.log(arr.includes(8));
+
+  // Exercise 14
+
+  // let str: string = "JavaScript"
+  // str = str.slice(0,4)
+  // console.log(str);
+  
+
+  // Exercise 15
+
+  // let arr : number[] = [1,2,3,4,5,6]
+  // let newArray : number[] = arr.filter((num) => {
+  //   return num % 2 === 0
+  // })
+  // console.log(newArray);
+
+  // Exercise 16
+
+  // let str : string = "Hello World"
+  // console.log(str.includes("world"));
+
+  // Exercise 17
+
+  // let arr: string[] = ["banana","apple","pear","grapes","cherry"]
+  // console.log(arr.indexOf("grapes"));
+
+
+  // Exercise 18
+
+  // let arr: number[] = [1,2,3,4,5]
+  // console.log(arr.slice(1,3));
+
+  // Exercise 19
+
+  //  
+  
+
+  return <></>;
+}
+
+export default App;
